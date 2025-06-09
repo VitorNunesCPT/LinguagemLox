e7b770eee7f849febdbd6b1a66c4fe146b80e1f0
João Pedro <devjonpedu@gmail.comm>
2025-06-09 14:35:44 -0300

    feat: Update class Scanner

--- Scanner.java
+++ Scanner.java
@@ -82,9 +82,28 @@ class Scanner {
         case '"': string(); break;
         
         default:
+            if (isDigit(c)) {
+            number();
+            } else {
             Lox.error(line, "Unexpected character.");
-            break;
+            }
+        break;
         }
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+        // Consume the "."
+        advance();
+
+        while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER,
+            Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
@@ -118,4 +137,13 @@ class Scanner {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
     }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    } 
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    } 
 }

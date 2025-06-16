18a82626820c5cf4931098a3ba119e4bbddfe85c
João Pedro <devjonpedu@gmail.comm>
2025-06-16 17:07:19 -0300

    refactor: method main

--- GenerateAst.java
+++ GenerateAst.java
@@ -6,11 +6,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
-  public static void main(String[] args) throws IOException {
-    if (args.length != 1) {
-      System.err.println("Usage: generate_ast <output directory>");
-      System.exit(64);
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"));
     }
-    String outputDir = args[0];
-  }
 }

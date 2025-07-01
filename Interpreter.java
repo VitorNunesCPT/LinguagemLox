323c94c6cfd3b783fbf6e154af9e6d8905c76a9b
Joao Pedro <devjonpedu@gmail.com>
2025-06-30 22:45:28 -0300

    refactor: Melhora a formatação do código e adiciona implementação do método visitBinaryExpr

--- Interpreter.java
+++ Interpreter.java
@@ -1,5 +1,5 @@
 class Interpreter implements Expr.Visitor<Object> {
-    
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -13,7 +13,7 @@ class Interpreter implements Expr.Visitor<Object> {
             case BANG:
                 return !isTruthy(right);
             case MINUS:
-                return -(double)right;
+                return -(double) right;
         }
 
         // Unreachable.
@@ -21,8 +21,10 @@ class Interpreter implements Expr.Visitor<Object> {
     }
 
     private boolean isTruthy(Object object) {
-        if (object == null) return false;
-        if (object instanceof Boolean) return (boolean)object;
+        if (object == null)
+            return false;
+        if (object instanceof Boolean)
+            return (boolean) object;
         return true;
     }
 
@@ -33,5 +35,41 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+
+                break;
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+        }
+
+        // Unreachable.
+        return null;
     }
 }

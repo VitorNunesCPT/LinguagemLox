4716d8c0366ac2d3ee91994e40adbf58f41b200f
João Pedro <devjonpedu@gmail.comm>
2025-07-21 18:23:06 -0300

    add: adicionando suporte para chamadas de função na expressão

--- Parser.java
+++ Parser.java
@@ -233,8 +233,37 @@ class Parser {
             return new Expr.Unary(operator, right);
         }
 
-        return primary();
+        return call();
     }
+
+    private Expr finishCall(Expr callee) {
+        List<Expr> arguments = new ArrayList<>();
+        if (!check(TokenType.RIGHT_PAREN)) {
+        do {
+            arguments.add(expression());
+        } while (match(TokenType.COMMA));
+        }
+
+        Token paren = consume(TokenType.RIGHT_PAREN,
+                            "Expect ')' after arguments.");
+
+        return new Expr.Call(callee, paren, arguments);
+    }
+
+    private Expr call() {
+        Expr expr = primary();
+
+        while (true) { 
+        if (match(TokenType.LEFT_PAREN)) {
+            expr = finishCall(expr);
+        } else {
+            break;
+        }
+        }
+
+        return expr;
+    }
+
     private Expr primary() {
         if (match(TokenType.FALSE)) return new Expr.Literal(false);
         if (match(TokenType.TRUE)) return new Expr.Literal(true);

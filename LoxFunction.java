2035d3b868618a2b9bbca1e7f10392d8c479a675
VitorNunesCPT <vitornunescpt@gmail.com>
2025-07-26 14:02:23 -0300

    feat: add Call and Logical expression support in the AST and parser

--- LoxFunction.java
+++ LoxFunction.java
@@ -8,32 +8,33 @@ class LoxFunction implements LoxCallable {
         this.closure = closure;
         this.declaration = declaration;
     }
-}
 
-@Override
-public String toString() {
-    return "<fn " + declaration.name.lexeme + ">";
-}
 
-@Override
-public int arity() {
-    return declaration.params.size();
-}
-
-@Override
-public Object call(Interpreter interpreter,
-        List<Object> arguments) {
-    Environment environment = new Environment(closure);
-    for (int i = 0; i < declaration.params.size(); i++) {
-        environment.define(declaration.params.get(i).lexeme,
-                arguments.get(i));
+    @Override
+    public String toString() {
+        return "<fn " + declaration.name.lexeme + ">";
     }
 
-    try {
-        interpreter.executeBlock(declaration.body, environment);
-    } catch (Return returnValue) {
-        return returnValue.value;
+    @Override
+    public int arity() {
+        return declaration.params.size();
     }
 
-    return null;
+    @Override
+    public Object call(Interpreter interpreter,
+                       List<Object> arguments) {
+        Environment environment = new Environment(closure);
+        for (int i = 0; i < declaration.params.size(); i++) {
+            environment.define(declaration.params.get(i).lexeme,
+                    arguments.get(i));
+        }
+
+        try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+            return returnValue.value;
+        }
+
+        return null;
+    }
 }

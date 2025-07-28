cf2f9488c0e042859877fd12eeca11e8488bc333
Joao Pedro <devjonpedu@gmail.com>
2025-07-28 03:27:48 -0300

    feat: Constructors and Initializers - add initializer support for class methods - enhance LoxFunction and LoxClass to handle initializers, update Resolver to recognize initializers in method declarations

--- Resolver.java
+++ Resolver.java
@@ -15,6 +15,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum FunctionType {
         NONE,
         FUNCTION,
+        INITIALIZER,
         METHOD
     }
     
@@ -101,6 +102,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
+            if (method.name.lexeme.equals("init")) {
+                declaration = FunctionType.INITIALIZER;
+            }
+            
             resolveFunction(method, declaration);
         }
 
@@ -140,6 +145,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             Lox.error(stmt.keyword, "Can't return from top-level code.");
         }
         if (stmt.value != null) {
+            if (currentFunction == FunctionType.INITIALIZER) {
+                Lox.error(stmt.keyword,
+                        "Can't return a value from an initializer.");
+            }
+
             resolve(stmt.value);
         }
 
@@ -198,7 +208,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             "Can't use 'this' outside of a class.");
         return null;
         }
-        
+
         resolveLocal(expr, expr.keyword);
         return null;
     }
